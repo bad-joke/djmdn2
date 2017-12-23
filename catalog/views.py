960a5cfcc6cec1cb6820f4a04d1a16ab53669846
Belvie Moore <belvie.moore@gmail.com>
2017-12-23 04:01:42 +0000

    start adding dat home page for my homies (etc)

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """
+    A barebones home page
+    """
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Available books
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.all().count()
+    
+    # Render the HTML
+    return render(
+        request,
+        'index.html', 
+        context = {'num_books':num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+    )
